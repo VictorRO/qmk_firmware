6630a9e47ce8c25b5965e4b3951dfd8be18646c2
Victor Rodriguez <victor@rodriguezortega.com>
2025-01-30 21:12:46 +0100

    Put everything in its right place

--- keyboards/boardsource/unicorne/keymaps/VictorRO/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/VictorRO/keymap.c
@@ -17,10 +17,10 @@ SPDX-License-Identifier: GPL-2.0-or-later
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
-    KC_ESC,   KC_Q,     KC_W,         KC_E,         KC_R,         KC_T,           KC_Y,     KC_U,         KC_I,         KC_O,           KC_P,     KC_BSPC,
-    KC_TAB,   KC_A,     LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,           KC_H,     RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L),   KC_QUOT,  KC_ENT,
-    KC_COLN,  KC_Z,     KC_X,         KC_C,         KC_V,         KC_B,           KC_N,     KC_M,         KC_COMM,      KC_DOT,         KC_SLSH,  KC_GRV,
-                                      C(KC_B),      MO(1),        KC_LSFT,        KC_SPC,   MO(2),        KC_DEL
+    KC_ESC,   KC_Q,           KC_W,         KC_E,         KC_R,         KC_T,           KC_Y,     KC_U,         KC_I,         KC_O,           KC_P,             KC_BSPC,
+    KC_TAB,   LSFT_T(KC_A),   LALT_T(KC_S), LCTL_T(KC_D), LGUI_T(KC_F), KC_G,           KC_H,     RGUI_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L),   RSFT_T(KC_QUOT),  KC_ENT,
+    KC_COLN,  KC_Z,           KC_X,         KC_C,         KC_V,         KC_B,           KC_N,     KC_M,         KC_COMM,      KC_DOT,         KC_SLSH,          KC_GRV,
+                                            C(KC_B),      MO(1),        KC_LSFT,        KC_SPC,   MO(2),        KC_DEL
   ),
 
   [1] = LAYOUT_split_3x6_3(
